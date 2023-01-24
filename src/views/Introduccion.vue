7bce309652ce4db96f4b019f74a1415d93a28942
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-01-23 19:25:24 -0500

    Se vinculan los videos.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -24,8 +24,8 @@
     .row
       figure
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video: Leyenda del video
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/h4Oi6DbVba8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        figcaption Video: Gestión de portafolio de productos alimenticios
 
 
     
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -170,8 +170,8 @@
 
     figure.mb-5
       .video.mt-5
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video  
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/sNWAZ5_MpW0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video: Propiedades organolépticas  
 
 
     .row.justify-content-center
